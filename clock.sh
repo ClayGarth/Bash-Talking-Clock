3ae2362acee30c70b301570004b1d65a258a2497
Clay <ClayGarth@users.noreply.github.com>
2019-01-25 22:53:20 -0500

    Add files via upload

--- clock.sh
+++ clock.sh
@@ -10,86 +10,86 @@ dir=/home/$user/clock/
 HOUR=$(date +"%I")
 if [ "$HOUR" = "01" ];
    then
-       /usr/bin/aplay -q $dir/1.wav && /usr/bin/aplay $dir/00.wav 
-   
+       /usr/bin/aplay -q $dir/1.wav && /usr/bin/aplay -q $dir/00.wav 
+      exit 0
 fi
 
 
 if [ "$HOUR" = "02" ];
    then
-       /usr/bin/aplay -q $dir/2.wav && /usr/bin/aplay $dir/00.wav 
-   
+       /usr/bin/aplay -q $dir/2.wav && /usr/bin/aplay -q $dir/00.wav 
+      exit 0
 fi
 
 
 if [ "$HOUR" = "03" ];
    then
-       /usr/bin/aplay -q $dir/3.wav && /usr/bin/aplay $dir/00.wav 
-   
+       /usr/bin/aplay -q $dir/3.wav && /usr/bin/aplay -q $dir/00.wav 
+      exit 0
 fi
 
 
 if [ "$HOUR" = "04" ];
    then
-       /usr/bin/aplay -q $dir/4.wav && /usr/bin/aplay $dir/00.wav 
-   
+       /usr/bin/aplay -q $dir/4.wav && /usr/bin/aplay -q $dir/00.wav 
+      exit 0
 fi
 
 
 if [ "$HOUR" = "05" ];
    then
-       /usr/bin/aplay -q $dir/5.wav && /usr/bin/aplay $dir/00.wav 
-   
+       /usr/bin/aplay -q $dir/5.wav && /usr/bin/aplay -q $dir/00.wav 
+      exit 0
 fi
 
 
 if [ "$HOUR" = "06" ];
    then
-       /usr/bin/aplay -q $dir/6.wav && /usr/bin/aplay $dir/00.wav 
-   
+       /usr/bin/aplay -q $dir/6.wav && /usr/bin/aplay -q $dir/00.wav 
+      exit 0
 fi
 
 
 if [ "$HOUR" = "07" ];
    then
-       /usr/bin/aplay -q $dir/7.wav && /usr/bin/aplay $dir/00.wav 
-   
+       /usr/bin/aplay -q $dir/7.wav && /usr/bin/aplay -q $dir/00.wav 
+      exit 0
 fi
 
 
 if [ "$HOUR" = "08" ];
    then
-       /usr/bin/aplay -q $dir/8.wav && /usr/bin/aplay $dir/00.wav 
-   
+       /usr/bin/aplay -q $dir/8.wav && /usr/bin/aplay -q $dir/00.wav 
+      exit 0
 fi
 
 
 if [ "$HOUR" = "09" ];
    then
-       /usr/bin/aplay -q $dir/9.wav && /usr/bin/aplay $dir/00.wav 
-   
+       /usr/bin/aplay -q $dir/9.wav && /usr/bin/aplay -q $dir/00.wav 
+      exit 0
 fi
 
 
 if [ "$HOUR" = "10" ];
    then
-       /usr/bin/aplay -q $dir/10.wav && /usr/bin/aplay $dir/00.wav 
-   
+       /usr/bin/aplay -q $dir/10.wav && /usr/bin/aplay -q $dir/00.wav 
+      exit 0
 fi
 
 
 if [ "$HOUR" = "11" ];
    then
-       /usr/bin/aplay -q $dir/11.wav && /usr/bin/aplay $dir/00.wav 
-   
+       /usr/bin/aplay -q $dir/11.wav && /usr/bin/aplay -q $dir/00.wav 
+      exit 0
 fi
 
 
 if [ "$HOUR" = "12" ];
    then
-       /usr/bin/aplay -q $dir/12.wav && /usr/bin/aplay $dir/00.wav 
-   
+       /usr/bin/aplay -q $dir/12.wav && /usr/bin/aplay -q $dir/00.wav 
+      exit 0
 fi
 
 
-exit
+exit 1
